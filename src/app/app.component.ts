c62665f741c1bd600cb3b6c4eccff18c52be11bf
Priyanshu Sharma <priyanshusharma72002@gmail.com>
2024-02-19 18:25:10 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Email_Notification_Feature';
+}
