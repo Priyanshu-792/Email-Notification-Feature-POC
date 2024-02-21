46f0674803752e53b7ca24cee68718259d4ca67c
Priyanshu Sharma <85648078+Priyanshu-792@users.noreply.github.com>
2024-02-21 22:47:38 +0530

    Added .net folder for Backend
    It uses mailKit as an email delivery service for notification.

---
+++ AutomatedEmailNotification/AutomatedEmailNotification/Services/EmailService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using AutomatedEmailNotification.Models;
+
+namespace AutomatedEmailNotification.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
