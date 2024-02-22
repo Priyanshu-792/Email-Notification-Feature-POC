126c2cde1b1b7bedcc5a8bc51df2a29f4c69646f
Priyanshu Sharma <85648078+Priyanshu-792@users.noreply.github.com>
2024-02-23 00:00:30 +0530

    Removing to add improved backend folder

--- AutomatedEmailNotification/AutomatedEmailNotification/Models/EmailDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutomatedEmailNotification.Models
-{
-    public class EmailDto
-    {
-        public string To { get; set; } = string.Empty;
-       // public string Subject { get; set; } = string.Empty;
-        public string Body { get; set; } = string.Empty;
-    }
-}
--- AutomatedEmailNotification/AutomatedEmailNotification/Services/EmailService/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using AutomatedEmailNotification.Models;
-
-namespace AutomatedEmailNotification.Services.EmailService
-{
-    public interface IEmailService
-    {
-        void SendEmail(EmailDto request);
-    }
-}
